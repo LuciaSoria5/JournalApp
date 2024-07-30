1873139016a84f232e52ddd9d675430c6dffeb85
Lucia Soria <luciacsoria5@gmail.com>
2024-07-30 14:56:29 -0300

    First commit - Adding Routes

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+import { JournalRoutes } from '../journal/routes/JournalRoutes'
+import { AuthRoutes } from '../auth/routes/AuthRoutes'
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+        {/* login y registro */}
+        <Route path="auth/*" element={ <AuthRoutes /> } />
+
+        {/* JournalApp */}
+        <Route path="/*" element={ <JournalRoutes /> } />
+
+    </Routes>
+  )
+}
