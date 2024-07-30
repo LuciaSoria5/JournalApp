fc4817c27c6c4750208d7675628ce36f8983b74a
Lucia Soria <luciacsoria5@gmail.com>
2024-07-30 16:37:53 -0300

    Add new view: NothingSelectedView

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,11 @@
-import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView } from "../views/NothingSelectedView"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
       
-      <Typography>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam nisi vero quibusdam amet doloribus eligendi ea unde animi nesciunt, repellat inventore, quam vitae impedit officiis distinctio eos possimus sapiente ratione.</Typography>
+      <NothingSelectedView />
 
     </JournalLayout>
   )
