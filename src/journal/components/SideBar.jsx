b1a8724fe57128e6a8e68c8dc3b91c86258f076f
Lucia Soria <luciacsoria5@gmail.com>
2024-07-30 16:28:41 -0300

    Add <SideBar /> component

---
+++ src/journal/components/SideBar.jsx
@@ -0,0 +1,51 @@
+import { TurnedInNot } from "@mui/icons-material"
+import { Box, Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from "@mui/material"
+
+export const SideBar = ({ drawerWidth = 240 }) => {
+  return (
+    <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+    >
+        <Drawer
+            variant="permanent" // podria ser temporary
+            open // open={ true }
+            sx={{
+                display: { xs: "block" },
+                "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth}
+            }}
+        >
+            <Toolbar>
+                <Typography
+                    variant="h6"
+                    noWrap
+                    component="div"
+                >
+                    Lucia Soria
+                </Typography>
+            </Toolbar>
+            <Divider />
+            
+            <List>
+                {
+                    ['Enero', 'Febreo', 'Marzo', 'Abril'].map( ( text ) => (
+                        <ListItem key={ text } disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <TurnedInNot />
+                                </ListItemIcon>
+                                <Grid container>
+                                    <ListItemText primary={ text }/>
+                                    <ListItemText secondary={ 'Ut eiusmod laboris et laborum cillum Lorem mollit enim fugiat aliquip ullamco reprehenderit.' }/>
+                                </Grid>
+                            </ListItemButton>
+                        </ListItem>
+                    ))
+                }
+            </List>
+
+        </Drawer>
+
+    </Box>
+  )
+}
