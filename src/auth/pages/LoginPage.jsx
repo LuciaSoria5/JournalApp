b5e48fb2ace7380c3afa4a34791721077376fda6
Lucia Soria <luciacsoria5@gmail.com>
2024-08-01 16:10:57 -0300

    Complete auth reducers and thunks, disable buttons

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,4 +1,5 @@
-import { useDispatch } from 'react-redux';
+import { useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link as RouerLink} from 'react-router-dom';
 
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
@@ -10,12 +11,16 @@ import { checkingAuthentication, startGoogleSignIn } from '../../store/auth';
 
 export const LoginPage = () => {
 
+  const { status } = useSelector( state => state.auth );
+
   const dispath = useDispatch();
 
   const { email, password, onInputChange } = useForm({
     email: 'luci@google.com',
     password: '123456'
   });
+
+  const isAuthenticating = useMemo( () => status === 'checking', [status] );
 
   const onSubmit = (event) => {
     event.preventDefault();
@@ -67,6 +72,7 @@ export const LoginPage = () => {
 
             <Grid item xs={ 12 } sm={ 6 }>
               <Button 
+                disabled= { isAuthenticating }
                 type="submit" 
                 variant="contained" 
                 fullWidth
@@ -76,7 +82,8 @@ export const LoginPage = () => {
             </Grid>
 
             <Grid item xs={ 12 } sm={ 6 }>
-              <Button 
+              <Button
+                disabled= { isAuthenticating }
                 variant="contained" 
                 fullWidth
                 onClick={ onGoogleSigIn }
