8102f9be0ca0ddc3e429c14e984bc1f7f284d160
Lucia Soria <luciacsoria5@gmail.com>
2024-08-01 14:33:18 -0300

    Configuring redux store and authSlice

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './styles.css';
+
 import { JournalApp } from './JournalApp';
+import { Provider } from 'react-redux';
+import { store } from './store';
+
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  // El estado debe esar en un punto alto de la aplicación
   <React.StrictMode>
-    <BrowserRouter>
-        <JournalApp/>
-    </BrowserRouter>
+    <Provider store={ store }>
+      <BrowserRouter>
+          <JournalApp/>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 )
